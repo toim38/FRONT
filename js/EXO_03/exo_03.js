76484bd86263d4c0836f0a53aeb4ed94336f4686
toimi68 <45996495+toimi68@users.noreply.github.com>
2019-01-31 16:43:14 +0100

    31/01/2019

---
+++ js/EXO_03/exo_03.js
@@ -0,0 +1,18 @@
+/*var a=3,b=2,x=8;
+var returnValeur=prompt.window((a*))
+function multiplication(){
+  document.write (2a+b)=x); 
+}*/
+/*-------------------CORRECTION---------------------------------------------*/
+
+var a=3;
+var b=2;
+function multiplie(x=8){
+  return 3*x;
+  }
+  function affiche(){
+  alerte (multiple(a));
+  alerte(multiplie(b));
+  alerte(multiplie());
+  }
+  
